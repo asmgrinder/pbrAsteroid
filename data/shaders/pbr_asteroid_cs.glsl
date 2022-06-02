0013f279499e0f1fe17c5105b49ca5202f214cf6
Victor Antoci <asmgrinder@gmail.com>
2022-06-02 15:18:12 +0300

    first commit

--- data/shaders/pbr_asteroid_cs.glsl
+++ data/shaders/pbr_asteroid_cs.glsl
@@ -59,9 +59,9 @@ void main()
         vec4 noise2 = height_map(normalize(position_cs_in[2]), START_LEVEL, MAX_LEVEL - 5, 1.0);
 
         vec4 pp[3];
-        pp[0] = modelViewMatrix * vec4(0.95 * height_mapping(noise0.a) * position_cs_in[0], 1.0);
-        pp[1] = modelViewMatrix * vec4(0.95 * height_mapping(noise1.a) * position_cs_in[1], 1.0);
-        pp[2] = modelViewMatrix * vec4(0.95 * height_mapping(noise2.a) * position_cs_in[2], 1.0);
+        pp[0] = modelViewMatrix * vec4(0.9 * height_mapping(noise0.a) * position_cs_in[0], 1.0);
+        pp[1] = modelViewMatrix * vec4(0.9 * height_mapping(noise1.a) * position_cs_in[1], 1.0);
+        pp[2] = modelViewMatrix * vec4(0.9 * height_mapping(noise2.a) * position_cs_in[2], 1.0);
 
         vec4 p0 = projectionMatrix * pp[0];
         vec4 p1 = projectionMatrix * pp[1];
@@ -73,7 +73,7 @@ void main()
         p1 *= sign(p1.w);
         p2 *= sign(p2.w);
 
-        const float mult = 1.4;
+        const float mult = 1.8;
         if (p0.x >  mult * p0.w && p1.x >  mult * p1.w && p2.x >  mult * p2.w
             || p0.x < -mult * p0.w && p1.x < -mult * p1.w && p2.x < -mult * p2.w
             || p0.y >  mult * p0.w && p1.y >  mult * p1.w && p2.y >  mult * p2.w
