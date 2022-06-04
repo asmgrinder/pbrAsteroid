3bad290b54b06aaca55cd2f19bf920f4dc1aa956
Victor Antoci <asmgrinder@gmail.com>
2022-06-04 21:23:40 +0300

    first commit

--- data/shaders/pbr_asteroid_fs.glsl
+++ data/shaders/pbr_asteroid_fs.glsl
@@ -105,7 +105,7 @@ void main()
 	{
 		discard;
 	}
-    float levelCount = clamp(20.15 + (log(koef_scr_diff_fs_in.y)) / log(2.0), START_LEVEL, MAX_LEVEL) - START_LEVEL;
+    float levelCount = clamp(20.45 + (log(koef_scr_diff_fs_in.y)) / log(2.0), START_LEVEL, MAX_LEVEL) - START_LEVEL;
 	float smoothing = exp(0.4 * log(koef_scr_diff_fs_in.x));
 	vec4 noise = height_map(normalize(mesh_pos_fs_in), START_LEVEL, levelCount, smoothing);
 
